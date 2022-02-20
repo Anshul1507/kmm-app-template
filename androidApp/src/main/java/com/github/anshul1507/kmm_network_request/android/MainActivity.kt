3357adda1f98279d8554863061588b294e5a2905
Anshul1507 <ag1507anshul@gmail.com>
2022-02-20 15:10:20 +0530

    response attached with viewmodel

--- androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/MainActivity.kt
+++ androidApp/src/main/java/com/github/anshul1507/kmm_network_request/android/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.github.anshul1507.kmm_network_request.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.github.anshul1507.kmm_network_request.Greeting
 import android.widget.TextView
-import com.github.anshul1507.kmm_network_request.repo.ModelRepo
-import kotlinx.coroutines.runBlocking
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.github.anshul1507.kmm_network_request.Greeting
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,9 +22,16 @@ class MainActivity : AppCompatActivity() {
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = greet()
 
-        runBlocking {
-            val response = ModelRepo().getListData()
-            Log.e("test", "result: " + response.list.count())
+        viewModel.getModelData().observe(this) {
+            if (it.isEmpty())
+                return@observe
+
+            val list = it
+            Log.e("test", "result: " + list.toString())
         }
+//        runBlocking {
+//            val response = ModelRepo().getListData()
+//            Log.e("test", "result: " + response.list.count())
+//        }
     }
 }
